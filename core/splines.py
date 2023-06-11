99feba721dde48d0f11d0d54cf3c39003b658885
david10238 <davidqcy02@gmail.com>
2023-06-10 19:19:32 -0500

    optimize 1d cubic generation

--- core/splines.py
+++ core/splines.py
@@ -6,22 +6,17 @@ from numpy.linalg import inv
 
 class CubicSpline1d:
     def __init__(self, a:float, b:float, c:float, d:float) -> None:
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+        self._a = a
+        self._b = b
+        self._c = c
+        self._d = d
         pass
-    def fit_spline(x0, x1, y0, y1, k0, k1)-> CubicSpline1d:
-        A = np.matrix([
-            [x0**3, x0**2, x0, 1],
-            [x1**3, x1**2, x1, 1],
-            [3*x0**2, 2*x0, 1, 0],
-            [3*x1**2, 2*x1, 1, 0]
-        ])
-        y = np.matrix([[y0, y1, k0, k1]]).transpose()
-        fit = np.matmul(inv(A), y)
-        [a, b, c, d] = fit
-        return CubicSpline1d(a, b, c, d)
+    def fit_spline(y0, y1, v0, v1)-> CubicSpline1d:
+        return CubicSpline1d(
+            a = 2*y0 - 2*y1 + v0 + v1,
+            b = -3*y0 + 3*y1 - 2*v0 - v1,
+            c = v0,
+            d = y0)
         
     def interp(self, x:float):
-        return self.a*x**3 + self.b*x**2 + self.c*x + self.d
+        return self._a*x**3 + self._b*x**2 + self._c*x + self._d
