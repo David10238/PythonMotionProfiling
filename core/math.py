196d8292e3f433baa4b37d24336f5ca6ed69e78b
david10238 <davidqcy02@gmail.com>
2023-06-10 23:21:25 -0500

    save a history of sums for the adaptive quadrature

--- core/math.py
+++ core/math.py
@@ -2,24 +2,39 @@ from typing import Callable
 import numpy as np
 from math import sqrt, exp
 
-EPS = 2.2e-10
+EPS = 2.2e-15
 
 # todo make list of results, to make parametization easier
-def integrate(a:float, b:float, f:Callable[[float], float], eps=EPS)->float:
+
+class IntegrationResult:
+    def __init__(self) -> None:
+        self.vals = [0.0]
+        self.sums = [0.0]
+        self.total_sum = 0.0
+
+def integrate(a:float, b:float, f:Callable[[float], float], eps=EPS)->IntegrationResult:
+    result = IntegrationResult()
     f = np.vectorize(f)
+
     def five_point(a:float, b:float)->float:
         points = np.array([0, (-1/3) * sqrt(5 - 2*sqrt(10/7)), (1/3) * sqrt(5 - 2*sqrt(10/7)), (-1/3) * sqrt(5 + 2*sqrt(10/7)), (1/3) * sqrt(5 + 2*sqrt(10/7))])
         weights = np.array([128 / 225, (322 + 13*sqrt(70))/900, (322 + 13*sqrt(70))/900, (322 - 13*sqrt(70))/900, (322 - 13*sqrt(70))/900])
         x = ((b-a)*points/2) + (a+b)/2
         return ((b-a)/2) * sum(f(x) * weights)
     
-    def helper(a:float, b:float, i:float)->float:
+    def helper(a:float, b:float, i:float, result:IntegrationResult)->float:
         m = (a+b) / 2
         i1 = five_point(a, m)
         i2 = five_point(m, b)
-
         if(abs(i - (i1 + i2)) <= eps):
-            return i1 + i2
-        return helper(a, m, i1) + helper(m, b, i2)
+            result.vals.append(m)
+            result.sums.append(result.sums[-1] + i1)
+            result.vals.append(b)
+            result.sums.append(result.sums[-1] + i2)
+        else:
+            helper(a, m, i1, result)
+            helper(m, b, i2, result)
     
-    return helper(a, b, five_point(a, b))
+    helper(a, b, five_point(a, b), result)
+    result.total_sum = result.sums[-1]
+    return result
